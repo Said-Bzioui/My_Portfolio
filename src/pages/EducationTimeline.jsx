758993d89dd63a4e005dacd1ed07f9af7f58013d
SAID BZIOUI <saidbzioui20@gmail.com>
2025-08-26 12:57:18 +0100

    Local data FIX

--- src/pages/EducationTimeline.jsx
+++ src/pages/EducationTimeline.jsx
@@ -2,30 +2,13 @@
 import { Phone, Sparkles } from "lucide-react";
 import Timeline from "../components/Timeline";
 import { Titele } from "../components/Headings";
+import { EDUCATIONS } from "../../lib/data";
 
 const EducationsPage = () => {
-    const educations = [
-        {
-            side: "left",
-            title: "Technicien Spécialisé En Développement Digital option web full stack",
-            date: "2023-2025",
-            description: "Cités des Métiers et des Compétences | Rabat",
-            photo: '/education/cmc.jpg'
-        },
-        {
-            side: "right",
-            title: "Baccalauréat Sciences Physiques",
-            date: "2021-2022",
-            description: "Zineb Nafzaouia | Sidi Slimane",
-
-        }
-    ];
-
-
     return (
         <div className="container mx-auto p-6" id="Educations">
             <Titele head="My Educations" paragraph="My Educational Journey To Develop My Coding Skills" />
-            <Timeline data={educations} />
+            <Timeline data={EDUCATIONS} />
         </div>
     );
 };
--- src/pages/ExperianceTimeline.jsx
+++ src/pages/ExperianceTimeline.jsx
@@ -2,23 +2,13 @@
 import { Phone, Sparkles } from "lucide-react";
 import Timeline from "../components/Timeline";
 import { Titele } from "../components/Headings";
+import { EXPERIANCES } from "../../lib/data";
 
 const ExperiencesPage = () => {
-    const experiences = [
-        {
-            side: "left",
-            title: "Développeur Web Full Stack (Stagiaire)",
-            date: "Avril–Juin 2025",
-            description: "DigiSense SARL – Rabat Agdal",
-            photo: '/education/cmc.jpg'
-        }
-    ];
-
-
     return (
         <div className="container mx-auto p-6" id="Experiences">
             <Titele head="My Experiences" paragraph="Experiences in Completing Individual And Group Projects" />
-            <Timeline data={experiences} />
+            <Timeline data={EXPERIANCES} />
         </div>
     );
 };
