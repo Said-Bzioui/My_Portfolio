14b53c54239082cac9fb891c4c3b14b78c0bac10
SAID BZIOUI <saidbzioui20@gmail.com>
2025-08-29 14:06:46 +0100

    fix certife section

--- src/pages/certificate.jsx
+++ src/pages/certificate.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect, memo, useMemo } from "react"
-import { FileText, Code, Award, Globe, ArrowUpRight, Sparkles, UserCheck } from "lucide-react"
+import React, { useEffect, memo, useState } from "react"
+import { FileText, Code, Award, Globe, ArrowUpRight, Sparkles, UserCheck, X, Fullscreen } from "lucide-react"
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { Titele } from "../components/Headings";
 
 
 const CertificatesPage = () => {
+  const [open, setOpen] = useState(false)
   // Optimized AOS initialization
   useEffect(() => {
     const initAOS = () => {
@@ -37,7 +38,7 @@ const CertificatesPage = () => {
     >
       <Titele head="My Certificates" paragraph="Validate My Skills And Continuous Learning Mindset" />
       <div className="container mx-auto flex justify-center items-center overflow-hidden">
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-5">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
           {[
             { id: 1, Title: "Project One", Description: "Description for project one.", Link: "https://example.com/1" },
             { id: 2, Title: "Project Two", Description: "Description for project two.", Link: "https://example.com/2" },
@@ -50,24 +51,49 @@ const CertificatesPage = () => {
               key={project.id || index}
               data-aos={index % 4 === 0 ? "fade-up-right" : index % 4 === 1 ? "fade-up" : "fade-up-left"}
               data-aos-duration={index % 4 === 0 ? "1000" : index % 4 === 1 ? "1200" : "1000"}
+              onClick={() => setOpen(project)}
             >
               {/* Certificate Image with Initial Filter */}
               <div className="relative overflow-hidden rounded-xl bg-gradient-to-br from-slate-900/90 to-slate-800/90 backdrop-blur-lg border border-white/10 shadow-2xl transition-all duration-300 hover:shadow-purple-500/20 p-2">
                 <img
-                  className="w-full h-auto block object-cover "
+                  className="w-full h-auto block object-cover"
                   src='/certifes/certife.jpg'
                   alt="Certificate"
                   style={{
                     filter: "contrast(1.10) brightness(0.8) saturate(1.1)",
                     transition: "filter 0.3s ease",
                   }}
-
                 />
+                <div className="absolute left-1/2 top-1/2 -translate-1/2  "><Fullscreen className="w-21" /></div>
+                <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent  flex flex-col justify-end p-2 md:p-4">
+                  <div className="mt-2 flex space-x-2">
+                    <span className="bg-purple-600 text-white text-xs px-2 py-1 rounded-full flex items-center space-x-1">
+                      <Award className="w-3 h-3" />
+                      <span >Certificate</span>
+                    </span>
+                  </div>
+                </div>
               </div>
-
             </div>
           ))}
         </div>
+        {open && (
+          <div className="w-full h-screen z-100 flex p-3 md:p-0 items-center justify-center bg-slate-800/80 fixed inset-0">
+            <div className="relative  border-[#a855f7]/50  bg-[#a855f7]/10  backdrop-blur-lg border  rounded-lg shadow-lg p-1 md:p-6 md:w-fit  w-full md:h-[80vh]">
+              <button
+                className="absolute -top-3 -right-3 bg-black/80 p-2 rounded-full text-[#a855f7] cursor-pointer hover:scale-110 transition-transform"
+                onClick={() => setOpen(false)}
+              >
+                <X />
+              </button>
+              <img
+                className="w-full h-full object-contain rounded-lg"
+                src='/certifes/certife.jpg'
+                alt={open.Title}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
