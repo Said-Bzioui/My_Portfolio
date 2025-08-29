e75edad049ca74514bb416d185af8cdd86499d69
SAID BZIOUI <saidbzioui20@gmail.com>
2025-08-29 15:13:28 +0100

    fix certife section _3

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -95,7 +95,6 @@ const ProjectStats = ({ project }) => {
           target="_blank"
           rel="noopener noreferrer"
           className="group relative inline-flex items-center space-x-1.5 md:space-x-2 px-4 md:px-8 py-2.5 md:py-4 bg-gradient-to-r from-purple-600/10 to-pink-600/10 hover:from-purple-600/20 hover:to-pink-600/20 text-purple-300 rounded-xl transition-all duration-300 border border-purple-500/20 hover:border-purple-500/40 backdrop-blur-xl overflow-hidden text-sm md:text-base"
-          onClick={(e) => !handleGithubClick(project.github) && e.preventDefault()}
         >
           <div className="absolute inset-0 translate-y-[100%] bg-gradient-to-r from-purple-600/10 to-pink-600/10 transition-transform duration-300 group-hover:translate-y-[0%]" />
           <Github className="relative w-4 h-4 md:w-5 md:h-5 group-hover:rotate-12 transition-transform" />
@@ -127,7 +126,7 @@ const ProjectDetails = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [project, setProject] = useState(null);
-  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const [picture, setPicture] = useState();
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -146,6 +145,13 @@ const ProjectDetails = () => {
       </div>
     );
   }
+
+  // click
+  const handlePictureClick = (picture) => {
+    // Handle picture click event
+    setPicture(picture)
+
+  };
 
   return (
     <div className="min-h-screen  px-[2%] sm:px-0 relative overflow-hidden">
@@ -214,16 +220,26 @@ const ProjectDetails = () => {
             </div>
 
             <div className="space-y-6 md:space-y-10 animate-slideInRight">
-              <div className="relative rounded-2xl overflow-hidden border border-white/10 shadow-2xl group">
-
-                <div className="absolute inset-0 bg-gradient-to-t from-[#140000] via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500" />
+              <div className="relative p-3 rounded-2xl overflow-hidden border border-white/10 shadow-2xl group">
                 <img
                   src={'/projects/dash.jpeg'}
                   alt={project.title}
-                  className="w-full md:h-96 object-cover transform transition-transform duration-700 will-change-transform group-hover:scale-105"
-                  onLoad={() => setIsImageLoaded(true)}
+                  className="w-full md:h-96 object-cover "
                 />
-                <div className="absolute inset-0 border-2 border-white/0 group-hover:border-white/10 transition-colors duration-300 rounded-2xl" />
+                <div className="flex mt-5 space-x-2">
+                  {project.pictures?.map((pic, i) => (
+                    <div key={i}
+                      onClick={() => handlePictureClick(pic)}
+                      className=""
+                    >
+                      <img
+                        src={'/projects/dash.jpeg'}
+                        alt={project.title}
+                        className={`object-cover rounded-md cursor-pointer hover:scale-105 transition-transform h-12  md:h-16  ${picture === pic ? 'ring-3 ring-purple-800' : ''}`}
+                      />
+                    </div>
+                  ))}
+                </div>
               </div>
 
               {/* Fitur Utama */}
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, memo, useMemo } from "react"
+import React, { useEffect, memo } from "react"
 import { FileText, Code, Award, Globe, ArrowUpRight, Sparkles, UserCheck } from "lucide-react"
 import AOS from 'aos'
 import 'aos/dist/aos.css'
