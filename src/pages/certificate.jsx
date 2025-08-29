e4b916907a6e41a50e2ff4820d753ea2bece2140
SAID BZIOUI <saidbzioui20@gmail.com>
2025-08-29 14:33:17 +0100

    fix certife section _2

--- src/pages/certificate.jsx
+++ src/pages/certificate.jsx
@@ -3,10 +3,12 @@ import { FileText, Code, Award, Globe, ArrowUpRight, Sparkles, UserCheck, X, Ful
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { Titele } from "../components/Headings";
+import { CERTIFICATES } from "../../lib/data";
 
 
 const CertificatesPage = () => {
   const [open, setOpen] = useState(false)
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
   // Optimized AOS initialization
   useEffect(() => {
     const initAOS = () => {
@@ -30,7 +32,12 @@ const CertificatesPage = () => {
       clearTimeout(resizeTimer);
     };
   }, []);
-
+  // click 
+  const handleClick = (e, photo) => {
+    setOpen(true)
+    e.stopPropagation();
+    setSelectedPhoto(photo);
+  }
   return (
     <div
       className="h-auto pb-[10%] text-white overflow-hidden px-[5%] sm:px-[5%] lg:px-[10%] mt-10 sm-mt-0"
@@ -39,25 +46,19 @@ const CertificatesPage = () => {
       <Titele head="My Certificates" paragraph="Validate My Skills And Continuous Learning Mindset" />
       <div className="container mx-auto flex justify-center items-center overflow-hidden">
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-          {[
-            { id: 1, Title: "Project One", Description: "Description for project one.", Link: "https://example.com/1" },
-            { id: 2, Title: "Project Two", Description: "Description for project two.", Link: "https://example.com/2" },
-            { id: 3, Title: "Project Three", Description: "Description for project three.", Link: "https://example.com/3" },
-            { id: 4, Title: "Project Four", Description: "Description for project four.", Link: "https://example.com/4" },
-            { id: 5, Title: "Project Five", Description: "Description for project five.", Link: "https://example.com/5" },
-            { id: 6, Title: "Project Six", Description: "Description for project six.", Link: "https://example.com/6" }
-          ].map((project, index) => (
+          {CERTIFICATES.map((project, index) => (
             <div
               key={project.id || index}
+              className="cursor-pointer"
               data-aos={index % 4 === 0 ? "fade-up-right" : index % 4 === 1 ? "fade-up" : "fade-up-left"}
               data-aos-duration={index % 4 === 0 ? "1000" : index % 4 === 1 ? "1200" : "1000"}
-              onClick={() => setOpen(project)}
+              onClick={(e) => handleClick(e, project.photo)}
             >
               {/* Certificate Image with Initial Filter */}
-              <div className="relative overflow-hidden rounded-xl bg-gradient-to-br from-slate-900/90 to-slate-800/90 backdrop-blur-lg border border-white/10 shadow-2xl transition-all duration-300 hover:shadow-purple-500/20 p-2">
+              <div className="relative h-32 md:h-45 overflow-hidden rounded-xl bg-gradient-to-br from-slate-900/90 to-slate-800/90 backdrop-blur-lg border border-white/10 shadow-2xl transition-all duration-300 hover:shadow-purple-500/20 p-2">
                 <img
-                  className="w-full h-auto block object-cover"
-                  src='/certifes/certife.jpg'
+                  className="w-full  block object-cover"
+                  src={project.photo}
                   alt="Certificate"
                   style={{
                     filter: "contrast(1.10) brightness(0.8) saturate(1.1)",
@@ -88,7 +89,7 @@ const CertificatesPage = () => {
               </button>
               <img
                 className="w-full h-full object-contain rounded-lg"
-                src='/certifes/certife.jpg'
+                src={selectedPhoto}
                 alt={open.Title}
               />
             </div>
