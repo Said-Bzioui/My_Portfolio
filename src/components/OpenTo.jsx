5a5edf02ac929263c141cb34cfa8f1818c56bcbb
SAID BZIOUI <saidbzioui20@gmail.com>
2025-08-31 17:27:37 +0100

    certifes

--- src/components/OpenTo.jsx
+++ src/components/OpenTo.jsx
@@ -2,8 +2,8 @@
 export const OpenTo = () => {
     return (
         <div className="fixed top-21 p-1 -right-1 w-44 h-9 text-sm rounded-l-md flex justify-center items-center z-30 border border-[#a855f7]/50 text-[#a855f7] bg-[#a855f7]/20   ">
-            <div className='absolute -top-1 -left-1 w-3 h-3 rounded-full bg-[#00e508] '></div>
-            <div className='absolute -top-1 -left-1 w-3 h-3 rounded-full bg-[#00e508]  animate-ping '></div>
+            <div className='absolute -top-1 -left-1 w-3 h-3 rounded-full bg-[#00e508]/60 '></div>
+            <div className='absolute -top-1 -left-1 w-3 h-3 rounded-full bg-[#00e508]/60  animate-ping '></div>
             I am available to work
         </div>
     )
